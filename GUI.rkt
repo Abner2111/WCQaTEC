5767287972e4a5a1da1a4b0c1f5bbdc6e5fdcd19
Apollo13-0 <calderon.ig08@estudiantec.cr>
2022-10-10 17:13:07 -0600

    field and scoreboard

--- GUI.rkt
+++ GUI.rkt
@@ -2,14 +2,23 @@
 
 (require racket/gui/base)
 
+(define scoreTeam1 0)
+(define scoreTeam2 0)
+
+; Creates window frame
 (define frame (new frame%
                    [label "Example"]
-                   [width 900]
+                   [width 640]
                    [height 500]))
+
+; Creates a canvas into the mainframe
 (new canvas% [parent frame]
              [paint-callback
               (lambda (canvas dc)
-                (send dc set-scale 3 3)
-                (send dc set-text-foreground "blue")
-                (send dc draw-text "prueba" 0 0))])
+                (send dc draw-bitmap(read-bitmap "Resources/field.jpg") 0 35 )
+                (send dc draw-text (~a "CRC  " scoreTeam1 " BRA  " scoreTeam2) 10 5)
+                )
+              ]
+             )
+
 (send frame show #t)
